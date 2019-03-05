b1603b59f180d98581adf2076ff6b65e51bfe720
Phuc-Dao <pcdao@ucsd.edu>
2019-03-04 19:37:06 -0800

    update

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 using namespace std;
 
+//Constructor initialzes everything
 Node::Node(string val){
     this->key = val;
     this->visited = false;
@@ -21,19 +22,19 @@ Node::Node(string val){
 }
 
 Graph::Graph(void)  {
-    //Initialize the graph
-
-
 }
-
+//Destructor
 Graph::~Graph(void) {
 }
 
-
-/* Add a node to the graph representing person with id idNumber and add a connection between two nodes in the graph. */
- 
-/* Read in relationships from an inputfile to create a graph */
-
+/*
+ * bool Graph::loadFromFile(const char * in_filename);
+ * Description: This function will load the graph and generate the graph.
+ * It reads the file one line at a time. 
+ * Paramaters:  const char* in_filename - The name of the file being read from
+ * Return:  True - The file was successfull read
+ *          False - There was an error reading the file
+ */
 bool Graph::loadFromFile(const char* in_filename) {
   ifstream infile(in_filename);
 
@@ -59,19 +60,19 @@ bool Graph::loadFromFile(const char* in_filename) {
         Node *n = new Node(record[0]);
         nodes.insert(pair<string, Node *>(record[0] , n));
     }
-
+    //Do the same thing for the second paramter
     if(nodes.find(record[1]) == nodes.end()){
         Node *n2 = new Node(record[1]);
         this->nodes.insert(pair<string, Node *>(record[1] , n2));
     }
     
-    //Inserting the nodes 
+    //Insert the nodes into the hashmap
     Node * curr = nodes[record[0]];
     Node * dest = nodes[record[1]];
     curr->neighbors.push_back(dest);
     dest->neighbors.push_back(curr);
   }
-
+  
   if (!infile.eof()) {
     cerr << "Failed to read " << in_filename << "!\n";
     return false;
@@ -81,7 +82,15 @@ bool Graph::loadFromFile(const char* in_filename) {
   return true;
 }
 
-//This will print to the outfile
+/*
+ * void Graph::printToFile(string out_file, string in_file);
+ * Description: THis method will read the input from infile and then print
+ * the shortest path to outfile. If the line read is not a valid number than
+ * it will skip over it. If there is no path then it will print a new line
+ * Paramters:   out_file - The name of the outfile
+ *              in_file - The name of the in file
+ * Return:      None
+ */
 void Graph::printToFile(string out_file, string in_filename){
     ifstream infile(in_filename);
     ofstream outfile(out_file);   
@@ -102,11 +111,9 @@ void Graph::printToFile(string out_file, string in_filename){
     if (record.size() != 2) {
       continue;
     }
-   
     
     bool dig1 = isDigit(record[0]);
     bool dig2 = isDigit(record[1]);
-
  
     //Check if they are not numbers
     if(dig1 == false || dig2 ==false){
@@ -120,105 +127,128 @@ void Graph::printToFile(string out_file, string in_filename){
         continue;
     }
  
+    //Get the shortest path from the nodes
     Node * from = nodes.at(record[0]);
     Node * to = nodes.at(record[1]);
-  bool inpath =  pathfinder(from, to);
+    bool inpath =  pathfinder(from, to);
 
+    //If there is no path then print a new line
     if(inpath == false){
         outfile << "\n";
-        reset();
+        reset(); //Reset the graph for the next query
         continue;
     }    
 
-    //Write to the file
+    //Dequeue from shortest path stack and print it to the files
     while(!shortestPath.empty()){
-        Node * top = shortestPath.top();
-        shortestPath.pop();
-        outfile << top->key;
-        //Only print space if not the last element in the shortest path
-        if(!shortestPath.empty()){
-            outfile << " ";
-        }
-    }
+      Node * top = shortestPath.top();
+      shortestPath.pop();
+      outfile << top->key; //Print the key to file
+      //Only print space if not the last element in the shortest path
+      if(!shortestPath.empty()){
+          outfile << " ";
+      }
+    }  
 
-    //Reset the graph
+    //Reset the graph for the next query
     reset();
     outfile << "\n";
-   }
+    }
 }
 
+/*
+ * void Graph::reset();
+ * Description: Function that resets the values of each node so we can perform
+ * the next query
+ * Paramters:   None
+ * return:      None
+ */
 void Graph::reset(){
-    std::set<Node *>::iterator it = cache.begin();
-    while(it != cache.end()){
-        Node * curr = *it;
-        curr->dist = -1;
-        curr->parent = NULL;
-        it++;
-    }
-    cache.clear(); 
-    shortestPath = stack<Node *>();
+  std::set<Node *>::iterator it = cache.begin();
+  while(it != cache.end()){
+    Node * curr = *it;
+    curr->dist = -1;
+    curr->parent = NULL;
+    it++;
+  }
+  cache.clear(); 
+  shortestPath = stack<Node *>();
 
 }
-
+/*
+ * Helper method that displays that prints out the nodes
+ */
 void Graph::printShortestPath(){
-    while(!(shortestPath.empty())){
-        Node * curr = shortestPath.top();
-        shortestPath.pop();
-        cout << curr->key << endl;
-    }
+  while(!(shortestPath.empty())){
+    Node * curr = shortestPath.top();
+    shortestPath.pop();
+    cout << curr->key << endl;
+  }
 }
 
-/* Implement pathfinder*/
+/*
+ * bool graph:: pathfinder(Node * from, Node * to);
+ * Description: Function taht calculates the shortest path between from and to
+ * paramters:   Node * to - The node we are starting at
+ *              Node * from - The destination Node
+ * Return:      true if the path exists, false otherwise
+ */
 bool Graph::pathfinder(Node* from, Node* to) {
+  
+  //Enqueue the first element  
+  queue<Node *> tovisit;
+  stack<Node *> returnVal;
+  from->dist = 0;
+  cache.insert(from);  //keep track of nodes that we will reset later
+  cache.insert(to);
+  tovisit.push(from);
     
-    queue<Node *> tovisit;
-    stack<Node *> returnVal;
-    from->dist = 0;
-    cache.insert(from);
-    cache.insert(to);
-    tovisit.push(from);
-    
-    while(!tovisit.empty()){
+  while(!tovisit.empty()){
    
-        Node * curr = tovisit.front();
-        tovisit.pop();
+    Node * curr = tovisit.front();
+    tovisit.pop();
         
-        if(curr == to){
-            while(curr->parent){
-                returnVal.push(curr);
-                curr = curr->parent;
-            }
+    if(curr == to){
+      while(curr->parent){
+        returnVal.push(curr);
+        curr = curr->parent;
+      }
 
-            returnVal.push(from);            
-            shortestPath = returnVal;
+      returnVal.push(from);            
+      shortestPath = returnVal;
 
-            return true;
-        }    
-            //Enqueue the unvisited neighbor nodes
-            for(int i =0; i < curr->neighbors.size(); i++){
-                    if(curr->neighbors[i]->dist == -1){
-                        curr->neighbors[i]->parent = curr;
-                        curr->neighbors[i]->dist = curr->dist + 1;
-                       // visited.push_back(curr->neighbors[i]); //Keeping track
-                        cache.insert(curr->neighbors[i]);
-                        tovisit.push(curr->neighbors[i]);
-                    } 
-            }   
-     }   
-    return false;  
+      return true;
+    }    
+    //Enqueue the unvisited neighbor nodes
+    for(size_t i =0; i < curr->neighbors.size(); i++){
+      if(curr->neighbors[i]->dist == -1){
+        curr->neighbors[i]->parent = curr;
+        curr->neighbors[i]->dist = curr->dist + 1;
+        cache.insert(curr->neighbors[i]); //Keep track of nodes we will reset
+        tovisit.push(curr->neighbors[i]);
+      } 
+    }   
+  }   
+  return false;  
 }
 
+/*
+ * bool Graph::isDigit(stirng s);
+ * Description: Checks if a string has only digits
+ * parmaters:   s - The string we will be checking
+ * Return;  True - The string is a digit
+ *          False - The string is not a digit
+ */
 bool Graph::isDigit(string s){
-    for(int i =0; i < s.size(); i++){
-        if(isdigit(s[i]) == false){
-            return false;
-        }
+  for(size_t i =0; i < s.size(); i++){
+    if(isdigit(s[i]) == false){
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 /* Implement social gathering*/
-//TODO
 void Graph::socialgathering(vector<string>& invitees, const int& k) {
 }
 
--- src/Graph.hpp
+++ src/Graph.hpp
@@ -24,30 +24,32 @@ class Graph {
     private:
         void reset();
 
-    public:
+        //Set that keep track of nodes that we will reset later
         set<Node *> cache;
+        
+        //Map that contains all the nodes in the graph
         map<string, Node *>  nodes;
-        Graph(void);
-  
-
+        
+        //The shortest path that we will be printing frtom
         stack<Node *> shortestPath;     
-        ~Graph(void);
-
-        bool loadFromFile(const char* in_filename);
-
-        bool pathfinder(Node* from, Node* to);
-    
-        void socialgathering(vector<string>& invitees, const int& k);
-
-        void printToFile(string outfile, string infile);
-    
 
         void printShortestPath();
 
         bool isDigit(string s);
+
+        bool pathfinder(Node* from, Node* to);
+    
+    public:
+        Graph(void);
+
+        ~Graph(void);
+
+        bool loadFromFile(const char* in_filename);
+
+        void socialgathering(vector<string>& invitees, const int& k);
+
+        void printToFile(string outfile, string infile);
+    
 };
-
-
-
 
 #endif  // GRAPH_HPP
