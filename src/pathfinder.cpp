d4a87246a687497b35a0be34a884a21c2f67bf7b
Phuc-Dao <pcdao@ucsd.edu>
2019-03-04 19:44:51 -0800

    wjakhfksjdhfjkshdfkjshdkjfhsdf

--- src/pathfinder.cpp
+++ src/pathfinder.cpp
@@ -6,9 +6,14 @@
 #include <string>
 #include <vector>
 #include "Graph.hpp"
-
 using namespace std;
 
+/*
+ * void usage(char * program_name);
+ * Description: Checks if the user netered the correct number of arguments;
+ * paramters: program_name - The command line paramter
+ * return:  None
+ */
 void usage(char* program_name) {
     cerr << program_name << " called with incorrect arguments." << endl;
     cerr << "Usage: " << program_name
@@ -17,6 +22,18 @@ void usage(char* program_name) {
     exit(-1);
 }
 
+/*
+ * int main(int argc, char* argv[);
+ * Description: Main driver function for the pathfinder program
+ * Takes in a graph file and generates a undirected graph. Then
+ * it takes a input file to query the longest path, and an out
+ * file to print the path two
+ * Paramters:   argv[1] - The graph file
+ *              argv[2] - The input query file
+ *              argv[3] - The output filea
+ * Return:  error if there was an error reading in the file
+ *          success if the function successfully executed
+ */
 int main(int argc, char* argv[]) {
   
     if (argc != 4) {
